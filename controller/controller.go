1b4ce6b052b3951b579e6f0ce49967fda50b4103
Carlos Eduardo Assis Rosa <carlos.eduardo@GENESIS.CORP>
2024-09-11 11:16:06 -0300

    modificado controller

--- controller/controller.go
+++ controller/controller.go
@@ -7,27 +7,29 @@ import (
 	"io"
 	"net/http"
 	"os"
+	_"embed"
 )
-func AddDados(w http.ResponseWriter, r *http.Request) {
 
+var (
+	//go:embed query/insert_query.sql
+	insert_query string
+)
+
+func AddDados(w http.ResponseWriter, r *http.Request) {
 	body, err := readRequestBody(r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Falha ao ler o corpo da requisição", http.StatusInternalServerError)
 		return
 	}
 
 	dados, formattedData, err := parseData(body)
 	if err != nil {
-		if err.Error() == "unsupported content type" {
-			http.Error(w, "Conteudo nao suportado", http.StatusUnsupportedMediaType)
-		} else {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
+		http.Error(w, "Falha ao fazer o parse dos dados", http.StatusInternalServerError)
 		return
 	}
 
 	if err := saveToDatabase(dados); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Falha ao salvar no banco de dados", http.StatusInternalServerError)
 		return
 	}
 
@@ -41,32 +43,21 @@ func AddDados(w http.ResponseWriter, r *http.Request) {
 }
 
 func readRequestBody(r *http.Request) ([]byte, error) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
+	return io.ReadAll(r.Body)
 }
 
 func parseData(body []byte) (models.Dados, string, error) {
 	var dados models.Dados
-	var formattedData string
 
 	if err := json.Unmarshal(body, &dados); err != nil {
 		return dados, "", err
 	}
-	formattedData = string(body)
 
-	return dados, formattedData, nil
+	return dados, string(body), nil
 }
 
 func saveToDatabase(dados models.Dados) error {
-	query := `INSERT INTO Dados (ds_operacao, ds_paciente, cd_paciente, dt_nascimento, ds_sexo, ds_accession_number, 
-	nr_identificador, ds_procedimento, ds_medico, ds_crm_nr, ds_crm_uf, bb_laudo, bb_rtf, dt_assinatura, dt_data,
-	medico_solicitante, codigo_procedimento, tipo_exame, modalidade)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`
-	_, err := db.DB.Exec(query, dados.Operacao, dados.NomePaciente, dados.PatientID, dados.DataNascimento, dados.Sexo, dados.AccessionNumber,
+	_, err := db.DB.Exec(insert_query, dados.Operacao, dados.NomePaciente, dados.PatientID, dados.DataNascimento, dados.Sexo, dados.AccessionNumber,
 		dados.IdentificadorUnico, dados.Procedimento, dados.MedicoRadiologista, dados.CRMNR, dados.CRMUF, dados.Laudo, dados.LaudoRTF, dados.DataAssinatura,
 		dados.DataExame, dados.Medico_solicitante, dados.Codigo_procedimento, dados.Tipo_exame, dados.Modalidade)
 	return err
